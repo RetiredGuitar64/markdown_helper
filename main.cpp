5eca3a3fb1061851a02aefb97d5a915e2616172c
RetiredGuitar64 <retiredguitar64@qq.com>
2026-09-17 15:35:59 +0800

    补一下运行时的基础配置

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int main(int argc, char *argv[])
     // 每个 Qt Widgets 程序都需要唯一的 QApplication 对象
     QApplication a(argc, argv);
 
+    // 应用名称会用于系统数据目录以及桌面环境中的显示
+    QApplication::setApplicationName(QStringLiteral("markdown_helper"));
+    QApplication::setApplicationDisplayName(QStringLiteral("Markdown 笔记管理器"));
+
     // 创建并显示应用的主窗口
     MainWindow w;
     w.show();
